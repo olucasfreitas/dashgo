3d4f971a730a2a868ec6181db9ff19b138e08d05
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-08-12 16:00:05 -0300

    criação e estilização do sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import Header from "../components/Hedaer";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1460} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
